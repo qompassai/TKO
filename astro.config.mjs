e151f52656f9d935b35d396d3036882645297054
phaedrusflow <map@qompass.ai>
2025-07-18 21:31:54 -0700

    Release metadata updates for 2025-07-18

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// /qompassai/tko/astro.config.mjs
+// Qompass AI TKO Astro Config
+//
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [tailwind(), icon(), mdx(), react(), svelte()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "github-light",
+				dark: "github-dark",
+			},
+		},
+	},
+});
