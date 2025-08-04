a35370a185208b089fabc210d10bf54ea9fc13ff
phaedrusflow <map@qompass.ai>
2025-08-03 18:23:02 -0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // /qompassai/tko/astro.config.mjs
 // Qompass AI TKO Astro Config
+// Copyright (C) 2025 Qompass AI, All rights reserved
+/////////////////////////////////////////////////////
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
@@ -12,9 +14,7 @@ import fs from "node:fs";
 export default defineConfig({
 	site: "https://qompassai.github.io/tko",
 	base: "/tko",
-
 	integrations: [tailwind(), icon(), mdx(), react(), svelte()],
-
 	markdown: {
 		shikiConfig: {
 			themes: {
